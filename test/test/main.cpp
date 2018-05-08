aab3507176765557f34fa62b7ba33009ea894b93
MunEop Kim <hub99op@gmail.com>
2018-05-08 20:40:36 +0900

    ㅇㅇㅇ
    ㅇㅇㅇ

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -13,11 +13,7 @@ using namespace std;
 int main(){
     cout<<"I'm HAYOUNG KIM!!!"<<endl;
 	cout << "I'm HJ Lee!!" << endl;
-<<<<<<< HEAD
 	cout<<"I'm MUNEOP Kim!!!!"<<endl;
 	cout<<"I'm Juhun Oh!"<<endl;
-=======
-	cout<<"I'm MUNEOP Kim!!"<<endl;
-	cout << "I'm Juhun Oh!!" << endl;
->>>>>>> 48f24c61bf43b3509c7dc11517247d0f2622d5ac
+    
 }
