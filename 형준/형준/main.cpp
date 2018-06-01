ea38b653b214a60371e8479e0d52819af037e4aa
hayoung <kimhayoung0311@gmail.com>
2018-06-01 14:52:33 +0900

    2

---
+++ 형준/형준/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  형준
+//
+//  Created by 김하영 on 2018. 6. 1..
+//  Copyright © 2018년 HAYOUNG_KIM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
