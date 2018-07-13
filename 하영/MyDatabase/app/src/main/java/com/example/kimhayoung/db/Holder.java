9c4b96893c9532ac2139e7b788b0df33d346b5af
hayoung <kimhayoung0311@gmail.com>
2018-07-14 05:06:14 +0900

    .
    .

---
+++ 하영/MyDatabase/app/src/main/java/com/example/kimhayoung/db/Holder.java
@@ -0,0 +1,10 @@
+package com.example.kimhayoung.db;
+
+import android.widget.TextView;
+
+class Holder {
+        public TextView tvId;
+        public TextView tvName;
+        public TextView tvAge;
+        public TextView tvPhone;
+}
