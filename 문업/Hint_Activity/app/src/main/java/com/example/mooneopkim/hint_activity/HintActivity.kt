1dd852dd4413a514ed6ba65e1e0713af107308c5
MunEop Kim <hub99op@gmail.com>
2018-08-15 02:26:00 +0900

    hint

---
+++ 문업/Hint_Activity/app/src/main/java/com/example/mooneopkim/hint_activity/HintActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mooneopkim.hint_activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class HintActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hint)
+    }
+}
