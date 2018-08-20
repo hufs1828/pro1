6b98ebcd64487d957bfc792a135c7af521ada2c6
OhJuhun <OhJuhun>
2018-08-20 21:00:40 +0900

    합본+구글맵 반경표시(100m)

---
+++ 합본/LoginActivity3/app/src/main/java/com/example/samsung/loginactivity/SampleIntent.java
@@ -0,0 +1,57 @@
+package com.example.samsung.loginactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
+public class SampleIntent extends AppCompatActivity implements View.OnClickListener {
+    private static final int COURSE_SELECT = 9003;
+    private static final int MISSION_SELECT = 9004;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_intent);
+
+        findViewById(R.id.button_continue).setOnClickListener(this);
+        findViewById(R.id.button_new).setOnClickListener(this);
+        findViewById(R.id.button_rank).setOnClickListener(this);
+        findViewById(R.id.button_store).setOnClickListener(this);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void selectCourse(){
+        Intent intent = new Intent(SampleIntent.this,CourseSelect.class);
+        startActivityForResult(intent,COURSE_SELECT);
+    }
+    private void selectMission(){
+        Intent intent = new Intent(SampleIntent.this,MissionActivity.class);
+        startActivityForResult(intent,MISSION_SELECT);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_continue:
+                Intent intent = new Intent();
+                //intent.putExtra("name","mike");
+                Toast.makeText(getApplicationContext(),"이어하기",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button_new:
+                Toast.makeText(getApplicationContext(),"새로운 코스 선택!",Toast.LENGTH_LONG).show();
+                selectCourse();
+                break;
+            case R.id.button_rank:
+                selectMission();
+                Toast.makeText(getApplicationContext(),"RANK 준비중입니다.",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button_store:
+                Toast.makeText(getApplicationContext(),"STORE 준비중입니다.",Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
